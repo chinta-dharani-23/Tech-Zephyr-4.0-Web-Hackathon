bd8dd6e308632aa1e13b5547dcc602d2f6af6424
Chinta Dharani <dharanichintha23@gmail.com>
2026-09-12 18:26:10 +0530

    project architecture

---
+++ backend/src/main/java/com/lifeRPG/lifeRPG_backend/LifeRpgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lifeRPG.lifeRPG_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeRpgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeRpgBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lifeRPG/lifeRPG_backend/LifeRpgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifeRPG.lifeRPG_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeRpgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
